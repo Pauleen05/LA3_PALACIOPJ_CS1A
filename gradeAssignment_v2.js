19192c12b5b710a84e6126afb026c338b83b9dfc
Pauleen05 <palaciopauleenjoyce@gmail.com>
2025-03-28 23:59:48 +0800

    gradeAssignment_v2.js

--- gradeAssignment_v2.js
+++ gradeAssignment_v2.js
@@ -2,17 +2,17 @@
 let score = parseInt(prompt("Enter your score: "));
 
 // Assign equivalent grade using conditional ternary operators
-let grade = score >= 97 ? '1.00 Excellent' :
-    score >= 94 ? '1.25 Excellent' :
-    score >= 91 ? '1.50 Above Average' :
-    score >= 88 ? '1.75 Above Average' :
-    score >= 85 ? '2.00 Average' :
-    score >= 82 ? '2.25 Average' :
-    score >= 79 ? '2.50 Below Average' :
-    score >= 76 ? '2.75 Below Average' :
-    score === 75 ? '3.00 Below Average' :
-    score >= 72 ? '4.00 Poor' :
-    '5.00 Poor';
+let grade = score >= 97 ? "1.00 Excellent" :
+    score >= 94 ? "1.25 Excellent" :
+    score >= 91 ? "1.50 Above Average" :
+    score >= 88 ? "1.75 Above Average" :
+    score >= 85 ? "2.00 Average" :
+    score >= 82 ? "2.25 Average" :
+    score >= 79 ? "2.50 Below Average" :
+    score >= 76 ? "2.75 Below Average" :
+    score === 75 ? "3.00 Below Average" :
+    score >= 72 ? "4.00 Poor" :
+    "5.00 Poor";
 
 // Determine final remarks using if-else statements
 let remarks;
